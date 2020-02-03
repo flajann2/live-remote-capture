5dc3084811d8d9b5466cb5b35993f35fb1203169
Fred Mitchell <fred.mitchell@gmx.de>
2020-02-03 12:11:11 +0100

    catching everything up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
 //! Live Remote Capture -- core library of functions
 
-// pub mod something;
+#![allow(unused)]
+use std::convert::AsRef;
+use std::path::{PathBuf, Path};
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_int};
+use libc::{size_t, c_float, c_void};
+use ffmpeg_dev::sys::{
+    self,
+    AVFrame,
+    AVDictionary,
+    AVCodec,
+    AVCodecContext,
+    AVStream,
+    AVPacket,
+    AVFormatContext,
+    AVOutputFormat,
+    AVCodecParameters,
+    AVCodecParserContext,
+    AVMediaType,
+    AVMediaType_AVMEDIA_TYPE_UNKNOWN as AVMEDIA_TYPE_UNKNOWN,
+    AVMediaType_AVMEDIA_TYPE_VIDEO as AVMEDIA_TYPE_VIDEO,
+    AVMediaType_AVMEDIA_TYPE_AUDIO as AVMEDIA_TYPE_AUDIO,
+    AVMediaType_AVMEDIA_TYPE_DATA as AVMEDIA_TYPE_DATA,
+    AVMediaType_AVMEDIA_TYPE_SUBTITLE as AVMEDIA_TYPE_SUBTITLE,
+    AVMediaType_AVMEDIA_TYPE_ATTACHMENT as AVMEDIA_TYPE_ATTACHMENT,
+    AVMediaType_AVMEDIA_TYPE_NB as AVMEDIA_TYPE_NB,
+    AVFMT_NOFILE,
+    AVIO_FLAG_WRITE,
+    AVRounding_AV_ROUND_NEAR_INF as AV_ROUND_NEAR_INF,
+    AVRounding_AV_ROUND_PASS_MINMAX as AV_ROUND_PASS_MINMAX,
+    AVCodecID_AV_CODEC_ID_H264 as AV_CODEC_ID_H264,
+    AV_INPUT_BUFFER_PADDING_SIZE,
+};
+
 
 pub mod scan;
 pub mod stream;
