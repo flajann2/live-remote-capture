581393c5cbbdfa21f9d2ae9df4bcc08bcdcdfb06
Fred Mitchell <fred.mitchell@gmx.de>
2020-05-20 08:48:44 +0200

    final before roadtrip

--- src/scan.rs
+++ src/scan.rs
@@ -9,7 +9,7 @@ mod tests {
 
     #[test]
     fn test_scanning() {
-        let mut avo : AVOutputFormat{};
+        let mut avo : AVOutputFormat;
         unsafe {
             let p_avo : *mut AVOutputFormat = mem::transmute(&avo);
             let _av = av_output_video_device_next(p_avo);
